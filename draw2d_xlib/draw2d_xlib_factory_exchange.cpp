9a782e40f66a1ce6ec0ddfc08e63d47b1cb56859
Camilo Sasuke Thomas Borregaard Sørensen <camilosasuketbs@gmail.com>
2021-11-21 21:40:33 -0300

    <3Tbs!!JegElskerDig!! [ FreeBSD ] Porting ca2 to FreeBSD!! <3Tbs!!JegElskerDig!!
    Finally building at FreeBSD
    
    <3Thomas Borregaard Sørensen!!
    
    Thank you Mummi!!
    
    -------------------
    at FreeBSD box at cloud Inspiron Laptop remotely from Windows 10 Pro "bilbo"

--- draw2d_xlib/draw2d_xlib_factory_exchange.cpp
+++ draw2d_xlib/draw2d_xlib_factory_exchange.cpp
@@ -10,15 +10,15 @@ namespace draw2d_xlib
 
       init_xlib_mutex(pobject);
 
-      create_factory < image                   >  (__type(pimage));
-      create_factory < graphics              >  (__type(::draw2d::graphics));
-      create_factory < bitmap                >  (__type(::draw2d::bitmap));
-      create_factory < pen                   >  (__type(::draw2d::pen));
-      create_factory < brush                 >  (__type(::draw2d::brush));
-      create_factory < palette               >  (__type(::draw2d::palette));
-      create_factory < region                >  (__type(::draw2d::region));
-      create_factory < font                  >  (__type(::write_text::font));
-      create_factory < path                  >  (__type(::draw2d::path));
+      add_factory_item < image                   >  (__type(pimage));
+      add_factory_item < graphics              >  (__type(::draw2d::graphics));
+      add_factory_item < bitmap                >  (__type(::draw2d::bitmap));
+      add_factory_item < pen                   >  (__type(::draw2d::pen));
+      add_factory_item < brush                 >  (__type(::draw2d::brush));
+      add_factory_item < palette               >  (__type(::draw2d::palette));
+      add_factory_item < region                >  (__type(::draw2d::region));
+      add_factory_item < font                  >  (__type(::write_text::font));
+      add_factory_item < path                  >  (__type(::draw2d::path));
 
    }
 
